c003b48d33ac213bf6b1caa5c7eade3691c3c724
malouda0000 <hamam.hamza85@gmail.com>
2025-02-16 15:40:37 +0400

    adding more logic to the color converter

--- lib/core/func/hash_color_converter.dart
+++ lib/core/func/hash_color_converter.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
-Color hexToColor(String hex) {
-  hex = hex.replaceAll("#", ""); // Remove the '#' if present
-  if (hex.length == 6) {
+Color hexToColor(String? hex) {
+  if (hex == null) {
+    return Colors.transparent;
+  } else if (hex.length == 6) {
+    hex = hex.replaceAll("#", ""); // Remove the '#' if present
     hex = "FF$hex"; // Add full opacity if only RGB is provided
+    return Color(int.parse(hex, radix: 16));
+  } else {
+    return Colors.white;
   }
-  return Color(int.parse(hex, radix: 16));
 }
